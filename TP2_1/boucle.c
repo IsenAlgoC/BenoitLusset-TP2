e819f50f7eff96b88a41ebb4cd91099b46a58e06
Benoit LUSSET <benoit.lusset@isen.yncrea.fr>
2020-09-25 10:11:46 +0200

    Valeur demandée sur boucle FOR

--- TP2_1/boucle.c
+++ TP2_1/boucle.c
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 int main() {
 
-	int n = 100;
+	int n =100;
 	int i;
 	int sommeFOR = 0;
-	for (i = 1; i <= n; i += 1) {
+	unsigned short valeur_demandee;
+	printf("Entrez un n pour la boucle FOR\n");
+	scanf_s("%hu", &valeur_demandee);
+	for (i = 1; i <= valeur_demandee; i += 1) {
 		sommeFOR = sommeFOR + i;
 	}
-	printf("BOUCLE FOR :\nLa somme des nombres de 1 jusque n = %d vaut %d\n", n,sommeFOR);
+	printf("\nBOUCLE FOR :\nLa somme des nombres de 1 jusque n = %d vaut %d\n", valeur_demandee,sommeFOR);
 
 	int j=1;
 	int sommeWHILE = 0;
@@ -27,5 +31,24 @@ int main() {
 		k += 1;
 	} while (k <= n);
 	printf("\nBOUCLE DO :\nLa somme des nombres de 1 jusque n = %d vaut %d\n", n, sommeDO);
+
+	//il faut que somme+i<=65535
+	bool check = false;
+	int l ;
+	unsigned short int sommeMAX = 0;
+	int MAX = 1;
+	while (check == false) {
+		sommeMAX = 0;
+		for (l = 1; l <= MAX; l += 1) {
+			if (sommeMAX + l >= 65535) {
+				check = true;
+				break;
+			}
+			else{ sommeMAX = sommeMAX + l; }
+		}
+		if (check == false) { MAX += 1; }
+	}
+
+	printf("\nLe n limite avant d'atteindre la somme maximale est %d et la somme est %d\n", MAX, sommeMAX);
 
 }
