1c14740f4e91f58a373ef10da2e5b0a70655198c
Benoit LUSSET <benoit.lusset@isen.yncrea.fr>
2020-09-25 11:39:58 +0200

    Methode If OK

---
+++ Exo3/Exo3.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+	int annee = 10001;
+	while (annee > 10000) {
+		printf("\nDonnez Un entier naturel inférieur à 10 000\n");
+		scanf_s("%d", &annee);
+	}
+	if (annee % 4 == 0){
+		if (annee % 100 == 0 && !(annee % 400 == 0)) {
+			printf("\nAnnee non bissextile\n");
+		}
+		else { printf("\nAnnee bissextile\n"); }
+	}
+	else { printf("\nAnnee non bissextile\n"); }
+
+}
